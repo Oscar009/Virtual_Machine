45f09d904f8cdce3867dea8a708237465bbbb814
Oscar Noe Ortiz Barba <oortizbarba@gmail.com>
2021-10-25 08:34:29 -0500

    Add assembly to ALU

--- ALU.cpp
+++ ALU.cpp
@@ -2,20 +2,59 @@
 
 ALU::ALU() {}
 
-int ALU::Add(int value1, int value2) { return value1 + value2; }
+int ALU::Add(int value1, int value2)
+{
+    int res;
 
-int ALU::Rest(int value1, int value2) { return value1 + value2; }
+    __asm("addl %%ebx, %%eax;"
+          : "=a"(res)
+          : "a"(value1), "b"(value2));
 
-int ALU::Mult(int value1, int value2) { return value1 + value2; }
+    return res;
+}
 
-int ALU::Div(int value1, int value2) { return value1 + value2; }
+int ALU::Rest(int value1, int value2)
+{
+    int res;
 
-void ALU::Mov(int value, Register* r)
+    __asm("subl %%ebx, %%eax;"
+          : "=a"(res)
+          : "a"(value1), "b"(value2));
+
+    return res;
+}
+
+int ALU::Mult(int value1, int value2)
+{
+    int res;
+
+    __asm("imull %%ebx, %%eax;"
+          : "=a"(res)
+          : "a"(value1), "b"(value2));
+
+    return res;
+}
+
+int ALU::Div(int value1, int value2)
+{
+    int res;
+
+    __asm("movl $0x0, %%edx;"
+          "movl %2, %%eax;"
+          "movl %3, %%ebx;"
+          "idivl %%ebx;"
+          : "=a"(res), "=d"(rem)
+          : "g"(value1), "g"(value2));
+
+    return res;
+}
+
+void ALU::Mov(int value, Register *r)
 {
     r->setValue(value);
 }
 
-void ALU::Sto(int value, Register* r)
+void ALU::Sto(int value, Register *r)
 {
     r->setValue(value);
 }
--- asm.cpp
+++ asm.cpp
@@ -1,23 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
-    register int res;
-    __asm("movl $10, %%eax;"
-		 "movl	$4, %%ebx;"
-		 "subl %%ebx, %%eax;" : "=a" (res) 
-    );
 
-    printf("res = %i\n", res);
+      int res;
+      int value1 = 10, value2 = 3, rem;
+      /* __asm("movl $10, %%eax;"
+            "movl	$4, %%ebx;"
+            "subl %%ebx, %%eax;"
+            : "=a"(res));
 
-    register int value1, value2;
-    __asm("movl $10, %%eax;"
-		 "movl	$4, %%ebx;"
-		 "subl %%ebx, %%eax;" : "=a" (res)
-    );
+      printf("res = %i\n", res);
 
-    printf("res = %i\n", res);
+      __asm("movl $10, %%eax;"
+            "movl	$4, %%ebx;"
+            "addl %%ebx, %%eax;"
+            : "=a"(res));
 
-    return 0;
+      printf("add = %i\n", res);
+
+      __asm("movl $10, %%eax;"
+            "movl	$4, %%ebx;"
+            "imull %%ebx, %%eax;"
+            : "=a"(res));
+
+      printf("mult = %i\n", res); */
+
+      __asm("addl %%ebx, %%eax;"
+            : "=a"(res)
+            : "a"(value1), "b"(value2));
+      printf("\nadd = %i\n", res);
+
+      __asm("subl %%ebx, %%eax;"
+            : "=a"(res)
+            : "a"(value1), "b"(value2));
+      printf("sub = %i\n", res);
+      
+      __asm("imull %%ebx, %%eax;"
+            : "=a"(res)
+            : "a"(value1), "b"(value2));
+      printf("mult = %i\n", res);
+
+      __asm("movl $0x0, %%edx;"
+            "movl %2, %%eax;"
+            "movl %3, %%ebx;"
+            "idivl %%ebx;"
+            : "=a"(res), "=d"(rem)
+            : "g"(value1), "g"(value2));
+
+      printf("div = %i\n\n", res);
+
+      return 0;
 }
